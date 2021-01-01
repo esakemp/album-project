4f1f0eb2bb65e298ed98bc58cf1b30734455ffbe
esakemp <esa.kemppainen@helsinki.fi>
2021-01-01 18:58:34 +0200

    fix restart

--- restart.sh
+++ restart.sh
@@ -1,3 +1,5 @@
+docker-compose down
 docker system prune -a --volumes --force
+docker-compose build --no-cache
 docker-compose -f docker-compose.yml -f docker/dco.updater.yml up -d
 docker-compose logs -f
